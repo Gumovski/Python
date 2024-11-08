281d6e308d19ea06cbfb3b49b2c4ce148a8295e9
Андрей Боев <a.boev.it@gmail.com>
2024-11-08 15:27:36 +0300

    FIX: Исправление ДЗ

--- 2. Операции деления/Задачи/Секунды.py
+++ 2. Операции деления/Задачи/Секунды.py
@@ -1,22 +1,7 @@
 a = int(input())
-b = int(input())
-c = int(input())
+
+h = a // 3600
+m = a % 3600 // 60
 s = a % 60
-mi = (a - s) // 60
-m = mi % 60
-h = mi // 60
 
-s1 = b % 60
-mi1 = (b - s1) // 60
-m1 = mi1 % 60
-h1 = mi1 // 60
-
-s2 = c % 60
-mi2 = (c - s2) // 60
-m2 = mi2 % 60
-h2 = mi2 // 60
-
-print(h,m,s)
-print(h1,m1,s1)
-print(h2,m2,s2)
-
+print(f'{h}:{m}:{s}')
