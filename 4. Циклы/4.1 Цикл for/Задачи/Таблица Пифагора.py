35d32e4a54bc4e66619214c4f3fadd696e50fcc2
Gumovski <Gumovskijstepan@gmail.com>
2024-12-27 12:39:38 +0300

    Решение задач

--- 4. Циклы/4.1 Цикл for/Задачи/Таблица Пифагора.py
+++ 4. Циклы/4.1 Цикл for/Задачи/Таблица Пифагора.py
@@ -1,9 +1,5 @@
 a = int(input())
-for i in range(a):
-    for j in range(a):
-        if i * j == 0:
-            print(f"")
-        elif i * j < 10:
-            print(i * j, end = '  ')
-        else:
-            print(i * j, end=' ')
+for i in range(1, a):
+    for j in range(1, a):
+        print(f'{i * j:<3d}', end='')
+    print()
