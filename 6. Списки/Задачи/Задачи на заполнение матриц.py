99e82bc4b5124b13fe73273aca9daa865763c013
Gumovski <Gumovskijstepan@gmail.com>
2025-04-25 18:01:03 +0300

    Решение задач

---
+++ 6. Списки/Задачи/Задачи на заполнение матриц.py
@@ -0,0 +1,19 @@
+def multiply(n,m):
+    return [[i*j for i in range(n)] for j in range(m)]
+
+def chess_order(n, m):
+    return [[1 if (i + j) % 2 == 0 else 0 for i in range(n)] for j in range(m)]
+def cross(n):
+    return [[1 if (i == j or i + j == n - 1) else 0 for j in range(n)] for i in range(n)]
+
+
+
+###
+def print_matrix(matrix):
+    for row in matrix:
+        for i in row:
+            print(f'{i:<3d}', end='')
+        print()
+print_matrix(cross(8))
+###
+
---
+++ 6. Списки/Задачи/Сапёр.py
@@ -0,0 +1,4 @@
+n, m, k = list(map(int,input().split()))
+for i in range(k):
+    x, y = list(map(int,input().split()))
+for i in range()
