a4c27c580cb393f22d85f0a7c6e5d17efc9c5ea8
Gumovski <Gumovskijstepan@gmail.com>
2025-08-07 17:01:16 +0300

    Решение задач

--- 3. Условные конструкции/Задачи/Задача 3.py
+++ 3. Условные конструкции/Задачи/Задача 3.py
@@ -2,28 +2,7 @@ a1 = int(input())
 b1 = int(input())
 a2 = int(input())
 b2 = int(input())
-if a1 == a2 - 1 or a1 == a2 + 1  and b1 == b2 -1 or b1 == b2 + 1:
-    print('YES')
+if ((a2 - a1 == b2 - b1) or (a2 - a1 == -(b2 - b1))) and (a2 - a1 != 0 or b2 - b1 != 0):
+    print('Yes')
 else:
-    if a1 == a2 - 2 or a1 == a2 + 2  and b1 == b2 - 2 or b1 == b2 + 2:
-        print('YES')
-    else:
-        if a1 == a2 - 3 or a1 == a2 + 3  and b1 == b2 - 3 or b1 == b2 + 3:
-            print('YES')
-        else:
-            if a1 == a2 - 4 or a1 == a2 + 4  and b1 == b2 - 4 or b1 == b2 + 4:
-                print('YES')
-            else:
-                if a1 == a2 - 5 or a1 == a2 + 5  and b1 == b2 - 5 or b1 == b2 + 5:
-                    print('YES')
-                else:
-                    if a1 == a2 - 6 or a1 == a2 + 6 and b1 == b2 - 6 or b1 == b2 + 6:
-                        print('YES')
-                    else:
-                        if a1 == a2 -7 or a1 == a2 + 7  and b1 == b2 - 7 or b1 == b2 + 7:
-                            print('YES')
-                        else:
-                            if a1 == a2 - 8 or a1 == a2 + 8 and b1 == b2 - 8 or b1 == b2 + 8:
-                                print('YES')
-                            else:
-                                print('NO')
+    print('No')
--- 8. Строки/Из любой системы в любую.py
+++ 8. Строки/Из любой системы в любую.py
@@ -1,3 +1,18 @@
 s = input()
 k, m = map(int,input().split())
+result = ''
 allowed = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+dec = 0
+for i in s:
+    if i in allowed:
+        value = allowed.index(i)
+        dec =  (dec * k) + value
+
+while dec > 0:
+    result = result + allowed[dec % m]
+    dec = dec // m
+if s[0] == '-':
+    print(f'-{result[::-1]}')
+else:
+    print(result[::-1])
