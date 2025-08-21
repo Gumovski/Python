dbf05d7d7f818a4fe3820f560b97dfc2449ff30d
Gumovski <Gumovskijstepan@gmail.com>
2025-08-21 17:03:21 +0300

    Решение задач

--- 9. Словари/Англо-латинский словарь.py
+++ 9. Словари/Англо-латинский словарь.py
@@ -1,8 +1,29 @@
 n = int(input())
 d = {}
-
 for i in range(n):
     word = input()
-    string = word.split(' - ')
+    string = word.split('-')
     english = string[0]
-    latin = string[1]
+    latin = string[1].split(',')
+
+    for lat in latin:
+        #d[lat.strip()] = english.strip()
+        if lat not in d:
+            d[lat] = []
+        d[lat].append(english)
+
+latin_s = list(d.keys())
+latin_s.sort()
+
+print(len(latin_s))
+
+for lat in latin_s:
+    english_s = sorted(d[lat])
+
+    print(f"{lat} - {','.join(english_s)}")
+
+
+#3
+#apple - malum, pomum, popula
+#fruit - baca, bacca, popum
+#punishment - malum, multa
