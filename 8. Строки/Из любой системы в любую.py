0ebf9930c42eedf4b323abe8999d541df47ce614
Андрей Боев <a.boev.it@gmail.com>
2025-08-21 17:06:48 +0300

    Проверка ДЗ

--- 8. Строки/Из любой системы в любую.py
+++ 8. Строки/Из любой системы в любую.py
@@ -1,5 +1,5 @@
 s = input()
-k, m = map(int,input().split())
+k, m = map(int, input().split())
 result = ''
 allowed = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
@@ -7,12 +7,10 @@ dec = 0
 for i in s:
     if i in allowed:
         value = allowed.index(i)
-        dec =  (dec * k) + value
+        dec = (dec * k) + value
 
 while dec > 0:
     result = result + allowed[dec % m]
     dec = dec // m
-if s[0] == '-':
-    print(f'-{result[::-1]}')
-else:
-    print(result[::-1])
+
+print(result[::-1])
